adadf450e3176a61a09724c142b62d279be00c3c
pkhire <priyanka.khire@gmail.com>
2016-05-09 16:23:01 -0700

    Adding first 2 beginners files

---
+++ FirstRubyFile.rb.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+print "This is my first Ruby Lesson.\n"
+puts "This is my second line."
+print "And this is the third line with print "
+puts "fourth line with puts"
+#Please change the permissions of the file you save this in
+#To change the permission of the file
+# chmod 777 <file_name>
+
---
+++ VariablesAndAddition.rb.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+variable = "String Variable \n"
+print variable
+
+a = 6
+b = 4
+addition = a+b
+product = a**b
+puts addition
+puts product
