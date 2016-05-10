484f2590a9a3dce7af16fb8f94b6a6ec1f8214b3
pkhire <priyanka.khire@gmail.com>
2016-05-10 10:18:03 -0700

    Added string and array files

--- StringFormatting.rb
+++ StringFormatting.rb
@@ -7,7 +7,18 @@ puts LowerCaseStringVariable.downcase
 puts LowerCaseStringVariable.reverse
 puts LowerCaseStringVariable.length
 
+#String concat
+puts LowerCaseStringVariable + " and now i am concatinating the string \n"
+
+#String groupping
 puts LowerCaseStringVariable[0..10]
 puts LowerCaseStringVariable[15..24]
+puts LowerCaseStringVariable[2]
 
+#String Split
+puts puts LowerCaseStringVariable.split
+Date = "12/03/2016"
+puts Date.split("/")
+puts Date[0..5]
+puts Date[0...5]
 $end
---
+++ array.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+String_Array = ["abc", "def", "ghi", "jkl", "mno"]
+Mixed_Array = ["String", 'c', 2, 6-9]
+
+#Start from beginning
+puts String_Array[2]
+puts Mixed_Array[1]
+#Start from end
+puts String_Array[-2]
+puts Mixed_Array[-1]
+
+#Putting variable in string statement
+puts "This is a string **** #{String_Array[1]} **** and i am substuting a variable there"
