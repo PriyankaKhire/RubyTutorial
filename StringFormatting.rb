340c092d18482ef1e63e9fd573b5667098b7a129
pkhire <priyanka.khire@gmail.com>
2016-05-09 17:02:50 -0700

    Added String Formatting code

---
+++ StringFormatting.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+LowerCaseStringVariable = "some string in lower case"
+puts LowerCaseStringVariable.capitalize
+puts LowerCaseStringVariable.upcase
+puts LowerCaseStringVariable.downcase
+puts LowerCaseStringVariable.reverse
+puts LowerCaseStringVariable.length
+
+puts LowerCaseStringVariable[0..10]
+puts LowerCaseStringVariable[15..24]
+
+$end
