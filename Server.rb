0a8324507255700ad07a273ed8b9ddd9b37ab592
pkhire <priyanka.khire@gmail.com>
2016-05-12 10:51:05 -0700

    Modified the server file

--- Server.rb
+++ Server.rb
@@ -11,7 +11,7 @@ server = TCPServer.open(port)
 loop {
 	Thread.start(server.accept) do |client|
 		client.puts(Time.now.ctime) #Sent the current time to the client
-		client.puts "Tanay does not know the arjun story haha :D"
+		client.puts "Connection opened"
 		client.puts "Alright I am closing the connection now. Bye, Take care !"
 		client.close
 	end
